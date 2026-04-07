7e242e543f2a2e2442d92da9fabd96d692ce5cdf
EliteDiamonHawk <dylan.huang02@sjsu.edu>
2026-04-06 22:20:07 -0700

    implemented status buttons

--- src/content/main.tsx
+++ src/content/main.tsx
@@ -4,7 +4,6 @@ import { createRoot } from 'react-dom/client'
 import App from './views/App.tsx'
 
 console.log('[CRXJS] Hello world from content script!')
-alert("MAIN TSX LOADED")
 document.body.style.outline = "6px solid red"
 
 const container = document.createElement('div')
